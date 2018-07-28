87ed41cf2a6912670522efe8d3c7075b3738ab02
Jackson Gomes <jgomes@ceulp.edu.br>
2018-07-28 18:55:26 -0300

    CRUD completo, com telas

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
+## Versão em produção
+
+
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
@@ -25,3 +29,7 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Gerando a documentação
+
+A documentação deste projeto utiliza [compodoc](https://compodoc.app/). Para gerar ou atualizar a documentação execute `npm run build-docs`. Para gerar a documentação e acessá-la por meio do servidor local execute `npm run serve-docs` e acesse `http://localhost:8080`.
---
+++ src/app/noticia.model.ts
@@ -0,0 +1,90 @@
+/**
+ * Classe Noticia.
+ */
+export class Noticia {
+    /**
+     * O identificador da notícia
+     */
+    id: number;
+
+    /**
+     * O título da notícia
+     */
+    titulo: string;
+
+    /**
+     * O conteúdo da notícia
+     */
+    conteudo: string;
+
+    /**
+     * O nome do autor da notícia
+     */
+    autor: string;
+
+    /**
+     * O e-mail do autor da notícia
+     */
+    emailDoAutor: string;
+
+    /**
+     * A data da notícia
+     */
+    data: Date;
+
+    /**
+     * Construtor da classe. O código trata o valor do parâmetro `data` da seguinte forma:
+     * 
+     * a) se estiver definido, então:
+     *   * se for do tipo string, então usa o construtor de `Date` para criar um objeto `Date`
+     *   * senão, atribui diretamente
+     * 
+     * b) senão, atribui null
+     *
+     * @param id O identificador da notícia
+     * @param titulo O título da notícia
+     * @param conteudo O conteúdo da notícia
+     * @param autor O nome do autor da notícia
+     * @param emailDoAutor O e-mail do autor da notícia
+     * @param data A data da publicação da notícia
+     */
+    constructor(id: number, titulo: string, conteudo: string, autor: string, emailDoAutor: string, data: Date) {
+        this.id = id;
+        this.titulo = titulo;
+        this.conteudo = conteudo;
+        this.autor = autor;
+        this.emailDoAutor = emailDoAutor;
+        if (data) {
+            if (typeof data === 'string') {
+                this.data = new Date(data);
+            } else {
+                this.data = data;
+            }
+        } else {
+            this.data = null;
+        }
+    }
+
+    /**
+     * Consulta a situação da notícia para verificar se está publicada:
+     *
+     * a) se a data [de publicação] não está definida, então a notícia não está publicada;
+     * b) se a data [de publicação] é maior que a data atual, então a notícia não está publicada;
+     * c) se a data [de publicação] é menor ou igual que a data atual, então a notícia está publicada.
+     *
+     * @returns boolean True indica que a notícia está publicada, False cc.
+     */
+    estahPublicada(): boolean {
+        // checa se a data está definida
+        if (!this.data) {
+            return false;
+        }
+        // checa se a data é maior que a data atual
+        const dataAtual = new Date();
+        if (this.data > dataAtual) {
+            return false;
+        }
+        // casos esgotados, notícia publicada
+        return true;
+    }
+}
